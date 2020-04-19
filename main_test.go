7413907e5d626d53cdb006ed456cccc6b57df191
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-19 14:15:39 +0800

    Added test for main:
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

---
+++ main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/pennz/antlr_lifestyle/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetThings(t *testing.T) {
+	env, err := model.GetFakeEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pgenv, err := model.GetDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tests := []struct {
+		name    string
+		fileds  model.Env
+		want    int
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"GetDB_Fake", env, 200, false},
+		{"GetDB_postgres", pgenv, 403, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			router := setupRouter()
+			addModelFunc2Router(router, tt.fileds)
+
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/things", nil)
+			router.ServeHTTP(w, req)
+
+			log.Println(w.Body)
+			assert.Equal(t, tt.want, w.Code)
+		})
+	}
+}
+
+func TestPingRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}

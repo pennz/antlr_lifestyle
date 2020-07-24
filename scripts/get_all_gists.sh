5b84e79da56212868a82ac4bece8c63c711ddf08
Vincent Zhou <fireflysuccess@gmail.com>
2020-07-24 19:25:29 +0800

    Script put to one file
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- scripts/get_all_gists.sh
+++ scripts/get_all_gists.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl $1 | grep -o "https://gist[^>]*\.js" | xargs -I{} ./get_gist_content.sh {}
+get_gist_content () {
+	curl $1 | sed -n 's/.*gist-meta.*\(https[^\\]*\).*view raw.*/\1/p' | sed 's/github.com/githubusercontent.com/' | xargs -I{} wget {}
+}
+
+export -f get_gist_content
+curl $1 | grep -o "https://gist[^>]*\.js" | xargs -I{} bash -c 'get_gist_content {}'
--- scripts/get_gist_content.sh
+++ scripts/get_gist_content.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+curl $1 | grep -o "https://gist[^>]*\.js" | xargs -I{} ./get_gist_content.sh {}
+
 curl $1 | sed -n 's/.*gist-meta.*\(https[^\\]*\).*view raw.*/\1/p' | sed 's/github.com/githubusercontent.com/' | xargs -I{} wget {}
+

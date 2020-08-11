3e828025b40e03438197d0329ac6cdd9a853a2d3
Vincent Zhou <fireflysuccess@gmail.com>
2020-08-11 11:38:06 +0800

    Test SVG in flutter
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- app/startup_namer/lib/main.dart
+++ app/startup_namer/lib/main.dart
@@ -5,7 +5,8 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';  // 新增了这一行
+import 'package:english_words/english_words.dart'; // 新增了这一行
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(new MyApp());
 
@@ -16,9 +17,10 @@ class MyApp extends StatelessWidget {
 
     return new MaterialApp(
       title: appTitle,
-      theme: new ThemeData(          // 新增代码开始... 
+      theme: new ThemeData(
+        // 新增代码开始...
         primaryColor: Colors.white,
-      ),                             // ... 代码新增结束
+      ), // ... 代码新增结束
       //home: new RandomWords(),
       home: MyHomePage(title: appTitle),
     );
@@ -32,107 +34,110 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = new Set<WordPair>();   // 新增本行
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0); 
+  final Set<WordPair> _saved = new Set<WordPair>(); // 新增本行
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
     return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
 
-      // 对于每个建议的单词对都会调用一次 itemBuilder，
-      // 然后将单词对添加到 ListTile 行中
-      // 在偶数行，该函数会为单词对添加一个 ListTile row.
-      // 在奇数行，该函数会添加一个分割线的 widget，来分隔相邻的词对。
-      // 注意，在小屏幕上，分割线看起来可能比较吃力。
+        // 对于每个建议的单词对都会调用一次 itemBuilder，
+        // 然后将单词对添加到 ListTile 行中
+        // 在偶数行，该函数会为单词对添加一个 ListTile row.
+        // 在奇数行，该函数会添加一个分割线的 widget，来分隔相邻的词对。
+        // 注意，在小屏幕上，分割线看起来可能比较吃力。
 
-      itemBuilder: (BuildContext _context, int i) {
-        // 在每一列之前，添加一个1像素高的分隔线widget
-        if (i.isOdd) {
-          return new Divider();
-        }
+        itemBuilder: (BuildContext _context, int i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          if (i.isOdd) {
+            return new Divider();
+          }
 
-        // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整）
-        // 比如 i 为：1, 2, 3, 4, 5 时，结果为 0, 1, 1, 2, 2，
-        // 这可以计算出 ListView 中减去分隔线后的实际单词对数量
-        final int index = i ~/ 2;
-        // 如果是建议列表中最后一个单词对
-        if (index >= _suggestions.length) {
-        // ...接着再生成10个单词对，然后添加到建议列表
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整）
+          // 比如 i 为：1, 2, 3, 4, 5 时，结果为 0, 1, 1, 2, 2，
+          // 这可以计算出 ListView 中减去分隔线后的实际单词对数量
+          final int index = i ~/ 2;
+          // 如果是建议列表中最后一个单词对
+          if (index >= _suggestions.length) {
+            // ...接着再生成10个单词对，然后添加到建议列表
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 
-  @override                                  // 新增代码片段 - 开始 ... 
+  @override // 新增代码片段 - 开始 ...
   Widget build(BuildContext context) {
-    //final wordPair = new WordPair.random(); // 删掉 ... 
+    //final wordPair = new WordPair.random(); // 删掉 ...
     //return new Text(wordPair.asPascalCase); // ... 这两行
 
-    return new Scaffold (                   // 代码从这里... 
+    return new Scaffold(
+      // 代码从这里...
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
-        actions: <Widget>[      // 新增代码开始 ...
+        actions: <Widget>[
+          // 新增代码开始 ...
           new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-        ],   
+        ],
       ),
       body: _buildSuggestions(),
-    );   
-  }                                          // ... 新增的代码片段 - 结束
+    );
+  } // ... 新增的代码片段 - 结束
 
   // 新增代码开始
   void _pushSaved() {
     Navigator.of(context).push(
-      new MaterialPageRoute<void>(   // 新增如下20行代码 ...
-      builder: (BuildContext context) {
-        final Iterable<ListTile> tiles = _saved.map(
-          (WordPair pair) {
-            return new ListTile(
-              title: new Text(
-                pair.asPascalCase,
-                style: _biggerFont,
-              ),
-            );
-          },
-        );
-        final List<Widget> divided = ListTile
-          .divideTiles(
+      new MaterialPageRoute<void>(
+        // 新增如下20行代码 ...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
             context: context,
             tiles: tiles,
-          )
-          .toList();
-        return new Scaffold(         // 新增 6 行代码开始 ...
-          appBar: new AppBar(
-            title: const Text('Saved Suggestions'),
-          ),
-          body: new ListView(children: divided),
-        );                           // ... 新增代码段结束.
-      },
-      ),                           // ... 新增代码结束
+          ).toList();
+          return new Scaffold(
+            // 新增 6 行代码开始 ...
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          ); // ... 新增代码段结束.
+        },
+      ), // ... 新增代码结束
     );
   }
 
   Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);  // 新增本行
+    final bool alreadySaved = _saved.contains(pair); // 新增本行
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
-      trailing: new Icon(   // 新增代码开始 ...
+      trailing: new Icon(
+        // 新增代码开始 ...
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
-      ),                    // ... 新增代码结束
-      onTap: () {      // 增加如下 9 行代码...
+      ), // ... 新增代码结束
+      onTap: () {
+        // 增加如下 9 行代码...
         setState(() {
           if (alreadySaved) {
             _saved.remove(pair);
-          } else { 
-            _saved.add(pair); 
-          } 
+          } else {
+            _saved.add(pair);
+          }
         });
-      },               // ... 一直到这里
+      }, // ... 一直到这里
     );
   }
 }
@@ -174,6 +179,9 @@ class Photo {
 
 class MyHomePage extends StatelessWidget {
   final String title;
+	// Follow this tutorial https://medium.com/flutter-community/using-svg-in-flutter-3dcf7b1dd713
+	final String dogUrl = 'https://www.svgrepo.com/show/2046/dog.svg';
+	final String dogFoodUrl = 'https://www.svgrepo.com/show/3682/dog-food.svg';
 
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -183,15 +191,26 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: FutureBuilder<List<Photo>>(
-        future: fetchPhotos(http.Client()),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-
-          return snapshot.hasData
-              ? PhotosList(photos: snapshot.data)
-              : Center(child: CircularProgressIndicator());
-        },
+      body: Center(
+        child: FloatingActionButton(
+					onPressed: () {
+						print('Thanks');
+					},
+					child: SvgPicture.network(
+						dogFoodUrl,
+						semanticsLabel: 'Feed button',
+						placeholderBuilder: (context) => Icon(Icons.error),
+					),
+				),
+//        FutureBuilder<List<Photo>>(
+//        future: fetchPhotos(http.Client()),
+//        builder: (context, snapshot) {
+//          if (snapshot.hasError) print(snapshot.error);
+//
+//          return snapshot.hasData
+//              ? PhotosList(photos: snapshot.data)
+//              : Center(child: CircularProgressIndicator());
+//        }),
       ),
     );
   }
--- notes.md
+++ notes.md
@@ -48,7 +48,7 @@ So answer: just run scheduled tasks. Not go calling python... Not
 over-complicate it. Not easy to maintain.
 
 ## parse by python, and then send it back to our go API?
-DODODO
+DO DO DO
 
 ## Message queue or not
 Queue: let python handle inputed sentences, and put it to handled ones. The un-handled 
@@ -59,6 +59,40 @@ Anyway,  we need parse out our input, and put it to dataset. Or read it out from
 dataset, then back to dataset.
 
 ## for display
-Follow datadog agent? Go just call python module?
-
 We use Javascript thing to show graph (html tools is more available, interactive graphs)
+(just highcharts)
+
+# ideal
+- plan your day, and tracking the progress
+
+    [review for Reflectly](https://medium.com/@bigdchang/reflectly-product-analysis-cd584a2aa98a)
+    and the suggestion from this guy:
+Find out your user's end goals/wishes/ambitions. Ask questions that help them
+reflect about their progress and life. Create a best friend/father/mother
+figure in your user's life that he or she never had.
+
+Now, with journal entries and reflections that are relevant to a user's life
+and goals, you have a few new avenues to encourage habitual usage.
+
+- One, see progress. Mood tracking, but forget about the arbitrary smiley/blue faces. **Monitor my journey towards my goals**.
+- Two, memories. Bring up my thoughts from a week, a month ago. Show me how far I've progressed.
+- Three, blatantly ask what they could have done today to make their day even better. Then, ask them to do that tomorrow. And check the next day and ask about their progress again. Celebrate with them when they take a step forward. Raise them up when they fail.
+
+## learn from other application
+### things
+**todo**
+You main thinking tool
+- Today
+- Upcoming
+- Anytime
+- Someday ( some time in the future)
+**quick find**
+nice to have
+
+**goal tracking**
+- progress split
+- milestone
+- notes taking (automatically review)(GNU Cash like, accounting)
+
+## technically
+Put to restful API, easier for exchange
